229cbbfd97e3b49895a9b26acfe40c80891c2844
Joe <joegray2@gmail.com>
2020-07-31 11:18:59 +0100

    Connect to mongodb

--- server.js
+++ server.js
@@ -3,5 +3,14 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const port = 8000;
 const app = express();
+const connectionString = 'mongodb+srv://application:SsJ5T1DcKoNF11vW@cluster0.3gqmz.gcp.mongodb.net/application?retryWrites=true&w=majority'
 
-require('./routes')(app, {});app.listen(port, () => {  console.log('We are live on ' + port);});
+require('./routes')(app, {});app.listen(port, () => {
+  MongoClient.connect(connectionString, {
+    useUnifiedTopology: true
+  }, (err, client) => {
+    console.log(connectionString);
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+  })
+});
